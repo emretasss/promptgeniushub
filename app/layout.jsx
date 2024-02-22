8c7549269bbbbce45a7da1cd48bd1466aba0e985
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-22 12:53:18 +0300

    layout page setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import '@styles/globals.css'
-const layout = () => {
+
+export const metadata ={
+    title: "PromptGeniusHub",
+    description:"PromptGeniusHub is an AI-powered platform designed to foster creativity and intelligence. This hub provides users with prompts that ignite thoughts and inspire creativity. Explore PromptGeniusHub to express yourself, develop new ideas, and push the boundaries of your mind.",
+    url:'https://promptgeniushub.vercel.app',
+}
+
+
+const layout = () => { 
+
   return (
     <div>
       
