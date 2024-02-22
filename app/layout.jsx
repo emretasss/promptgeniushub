7de935d076a25371a74050e465bd5aaa5758d2d7
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-22 12:41:17 +0300

    layout page setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const page = () => {
+const layout = () => {
   return (
     <div>
-      page
+      
     </div>
   )
 }
 
-export default page
+export default layout
