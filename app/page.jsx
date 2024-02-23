0302c3bac1b2a9bf4197ce7b8167ef18e7241e2a
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-23 14:49:49 +0300

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,11 @@ const Home = () => {
         <h1 className='head_text text-center' >Discover & Share 
         <br className="max-md:hidden "/>
         <span className='orange_gradient text-center'>AI-powered Prompts</span></h1>
+        <p className='desc text-center'>PromptGeniusHub is an AI-powered platform designed to foster creativity and intelligence. This hub provides users with prompts that ignite thoughts and inspire creativity. Explore PromptGeniusHub to express yourself, develop new ideas, and push the boundaries of your mind</p>
+
     </section>
   )
 } 
 
 export default Home
+ 
