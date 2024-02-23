bd10adb312e75ab918fc5bec9fe2cb8587d356f4
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-23 14:44:14 +0300

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
-        <h1 className='head_text text-center' >Discover & Share </h1>
+        <h1 className='head_text text-center' >Discover & Share 
         <br className="max-md:hidden "/>
-        <span> </span>
+        <span className='orange_gradient text-center'>AI-powered Prompts</span></h1>
     </section>
   )
 } 
