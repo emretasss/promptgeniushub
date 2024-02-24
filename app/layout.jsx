303c1fcef043fe6964d350a74306aa45a9caff72
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-24 18:20:10 +0300

    layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React, { Children } from 'react'
 import '@styles/globals.css'
-
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 export const metadata ={
     title: "PromptGeniusHub",
     description:"PromptGeniusHub is an AI-powered platform designed to foster creativity and intelligence. This hub provides users with prompts that ignite thoughts and inspire creativity. Explore PromptGeniusHub to express yourself, develop new ideas, and push the boundaries of your mind.",
@@ -19,6 +20,7 @@ const RootLayout = ({children}) => {
 
         </div>
         <main className='app' >
+          <Nav/>
             {children}
         </main>
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,12 +2,11 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { useState,useEffect } from 'react'
-import {signIn, signOut, useSession, getProviders,} from "next-auth/react"
 const Nav = () => {
   return (
    <nav className='flex-between w-full mb-16 pt-3'>
     <Link href="/" className='flex gap-2 flex-center'>
-      <Image src="logo.svg" alt="Logo" height={30} width={30} className=" object-contain "/>
+      <Image src="assets/images/logo.svg" alt="Logo" height={30} width={30} className="object-contain"/>
     </Link>
    </nav>
   )
