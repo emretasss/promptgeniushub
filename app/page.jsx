99ad090105becd547a67f3227778d08208db67b8
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-24 14:16:55 +0300

    layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Feed from '@components/Feed'
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
@@ -7,7 +7,7 @@ const Home = () => {
         <br className="max-md:hidden "/>
         <span className='orange_gradient text-center'>AI-powered Prompts</span></h1>
         <p className='desc text-center'>PromptGeniusHub is an AI-powered platform designed to foster creativity and intelligence. This hub provides users with prompts that ignite thoughts and inspire creativity. Explore PromptGeniusHub to express yourself, develop new ideas, and push the boundaries of your mind</p>
-
+    <Feed />
     </section>
   )
 } 
