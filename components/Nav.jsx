13c54bdd21a9e42bf3632e62fd9deb52b2278163
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-24 14:29:38 +0300

    layout

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,15 @@
+"use client";
 import Link from 'next/link'
 import Image from 'next/image'
 import { useState,useEffect } from 'react'
 import {signIn, signOut, useSession, getProviders,} from "next-auth/react"
 const Nav = () => {
   return (
-    <div>
-      
-    </div>
+   <nav className='flex-between w-full mb-16 pt-3'>
+    <Link href="/" className='flex gap-2 flex-center'>
+      <Image src="logo.svg" alt="Logo" height={30} width={30} className=" object-contain "/>
+    </Link>
+   </nav>
   )
 }
 
