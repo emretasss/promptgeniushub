e3a911f98e5ca7c139e1001b56466227b0c7b2d0
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-25 01:22:53 +0300

    Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,7 @@ import { useState,useEffect } from 'react'
 import {signIn,  signOut, useSession,getProviders} from 'next-auth/react'
 const Nav = () => {
   const isUserLoggedIn=true;
+  
 
   return (
    <nav className='flex-between w-full mb-16 pt-3'>
