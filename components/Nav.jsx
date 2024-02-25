07d22f29ae8f2c941b0541a24d1438b503d17e07
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-02-25 14:18:27 +0300

    Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,12 @@ import { useState,useEffect } from 'react'
 import {signIn,  signOut, useSession,getProviders} from 'next-auth/react'
 const Nav = () => {
   const isUserLoggedIn=false;
+  const [providers,setProviders ]=useState(null)
+  useEffect (()=>{
+    const setProviders = async ()=> {
 
+    }
+  },[])
 
   return (
    <nav className='flex-between w-full mb-16 pt-3'>
@@ -25,7 +30,7 @@ const Nav = () => {
     </div>
     ): (
   <>
-  
+
   </>
 
     ) }
