9aee878b4b426fbac7f6c99c585262adc10848e2
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-03-05 20:00:39 +0300

    provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { SessionProvider } from 'next-auth' 
+"use client ";
+import { SessionProvider } from 'next-auth/react' 
 const Provider = ( {children,session} ) => {
   return (
    <SessionProvider>
